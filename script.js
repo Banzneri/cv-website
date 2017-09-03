13bc4f692a73221428effab0ca318e4e22bef2e6
Pasi Hanhijärvi <pasi.hanhijarvi@gmail.com>
2017-09-03 03:12:44 +0300

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-
-    var folder = "images/";
-    $("body").append( "<img src='"+ folder + val +"'>" );
+    var folder = "http://localhost:8000/images/";
     $.ajax({
         url : folder,
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
                 if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                    $("body").append( "<img src='"+ folder + val +"'>" );
+                    $("#images1").append("<a class='image-link' href='" + folder + val + "'> <img src='"+ folder + val +"' class='flex-item''></a>");
                 }
-                $("h1").hide();
             });
         }
     });
